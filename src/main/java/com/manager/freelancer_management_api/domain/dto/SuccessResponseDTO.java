e5a5c389863f1438d4766ebc2ee210ddc3a5ac8f
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-04-08 18:02:15 -0300

    feat: add SuccessResponseDTO record

---
+++ src/main/java/com/manager/freelancer_management_api/domain/dto/SuccessResponseDTO.java
@@ -0,0 +1,15 @@
+package com.manager.freelancer_management_api.domain.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Schema(description = "DTO que representa mensagem de sucesso")
+public record SuccessResponseDTO(int statusCode,
+                                 String message,
+                                 LocalDateTime timestamp) {
+    public SuccessResponseDTO(String message) {
+        this(HttpStatus.OK.value(), message, LocalDateTime.now());
+    }
+}
