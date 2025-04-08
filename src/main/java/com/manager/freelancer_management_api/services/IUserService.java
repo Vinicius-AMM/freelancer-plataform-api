9ef4b2019df793204d3bf56da1d694350ac46e67
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-04-08 17:52:24 -0300

    feat: add IUserService interface

---
+++ src/main/java/com/manager/freelancer_management_api/services/IUserService.java
@@ -0,0 +1,17 @@
+package com.manager.freelancer_management_api.services;
+
+import com.manager.freelancer_management_api.domain.user.dto.response.UserProfileResponseDTO;
+import com.manager.freelancer_management_api.domain.user.entities.User;
+
+import java.util.UUID;
+
+public interface IUserService {
+    User getUser(UUID id);
+    UserProfileResponseDTO getUserProfile(UUID id);
+    void updateFullName(UUID id, String newName);
+    void updateEmail(UUID id, String password, String newEmail);
+    void updateDocument(UUID id, String password, String newDocument);
+    void updatePassword(UUID id, String oldPassword, String newPassword);
+    void changeUserRole(UUID id, String newUserRole);
+    void deleteById(UUID id, String userPassword);
+}
