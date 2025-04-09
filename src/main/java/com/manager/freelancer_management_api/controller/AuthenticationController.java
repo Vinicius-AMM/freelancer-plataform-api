2674afdd6fafba7957fe95acb8b74da0e90241de
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-04-09 15:31:56 -0300

    refactor: add and refactor examples to Swagger documentation

--- src/main/java/com/manager/freelancer_management_api/controller/AuthenticationController.java
+++ src/main/java/com/manager/freelancer_management_api/controller/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.manager.freelancer_management_api.controller;
 
 import com.manager.freelancer_management_api.domain.dto.ApiResponseDTO;
+import com.manager.freelancer_management_api.domain.dto.DTOValidationErrorResponse;
 import com.manager.freelancer_management_api.domain.user.dto.request.LoginRequestDTO;
 import com.manager.freelancer_management_api.domain.user.dto.request.RegisterUserRequestDTO;
 import com.manager.freelancer_management_api.domain.user.dto.response.LoginResponseDTO;
@@ -41,8 +42,8 @@ public class AuthenticationController {
             @ApiResponse(responseCode = "400", description = "Dados de login inválidos (falha no formato ou validação)",
                     content = @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = ApiResponseDTO.class),
-                            examples = @ExampleObject(value = "{\"statusCode\": 400, \"message\": \"Invalid email or password.\", \"timestamp\": \"2025-04-02T02:28:59.409Z\"}"))),
+                            schema = @Schema(implementation = DTOValidationErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"statusCode\": 400, \"errors\": {\"email\": \"Invalid email format\", \"password\": \"Fill in this field\"}, \"timestamp\": \"2025-04-02T02:28:59.409Z\"}"))),
             @ApiResponse(responseCode = "401", description = "Credenciais inválidas (email ou senha incorretos)",
                     content = @Content(
                             mediaType = "application/json",
@@ -57,17 +58,22 @@ public class AuthenticationController {
     @PostMapping("/register")
     @Operation(summary = "Registra o usuário", description = "Cria uma nova conta de usuário.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso"),
-            @ApiResponse(responseCode = "400", description = "Dados de registro inválidos (falha na validação do DTO)",
+            @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ApiResponseDTO.class),
-                            examples = @ExampleObject(value = "{\"statusCode\": 400, \"message\": \"Invalid request content.\", \"timestamp\": \"2025-04-02T02:28:59.409Z\"}"))),
+                            examples = @ExampleObject(value = "{\"statusCode\": 201, \"message\": \"Successful registration.\", \"timestamp\": \"2025-04-02T02:28:59.409Z\"}")
+                    )),
+            @ApiResponse(responseCode = "400", description = "Dados de registro inválidos (falha na validação do DTO)",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DTOValidationErrorResponse.class),
+                            examples = @ExampleObject(value = "{\"statusCode\": 400, \"errors\": {\"fullName\": \"must not be blank\", \"document\": \"Field must have just 11 or 14 characters.\", \"email\": \"Enter a valid email.\", \"password\": \"The password must have at least 6 characters.\", \"mainUserRole\": \"Main user role cannot be null.\"}, \"timestamp\": \"2025-04-02T02:28:59.409Z\"}"))),
             @ApiResponse(responseCode = "409", description = "Endereço de e-mail inválido ou já cadastrado.",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ApiResponseDTO.class),
-                            examples = @ExampleObject(value = "{\"statusCode\": 409, \"message\": \"Email already exists.\", \"timestamp\": \"2025-04-02T02:28:59.409Z\"}")))
+                            examples = @ExampleObject(value = "{\"statusCode\": 409, \"message\": \"Invalid email address.\", \"timestamp\": \"2025-04-02T02:28:59.409Z\"}")))
     })
     public ResponseEntity<ApiResponseDTO> register(@RequestBody @Valid RegisterUserRequestDTO registrationData){
         authenticationService.register(registrationData);
