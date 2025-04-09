811739509a13b0100ba310d39edfa0a9adb1344f
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-04-09 15:21:43 -0300

    feat: add email validation

--- src/main/java/com/manager/freelancer_management_api/domain/user/dto/request/EmailUpdateRequestDTO.java
+++ src/main/java/com/manager/freelancer_management_api/domain/user/dto/request/EmailUpdateRequestDTO.java
@@ -1,8 +1,12 @@
 package com.manager.freelancer_management_api.domain.user.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 
 @Schema(description = "DTO para atualização do email do usuário")
-public record EmailUpdateRequestDTO(@Schema(example = "password") String password,
-                                    @Schema(example = "newemail@test.com") String email) {
+public record EmailUpdateRequestDTO(@Schema(example = "password")
+                                    String password,
+                                    @Schema(example = "newemail@test.com")
+                                    @Email(message = "Invalid email format")
+                                    String email) {
 }
