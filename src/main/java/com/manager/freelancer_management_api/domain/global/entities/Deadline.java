814aa38d147d6ca72dcf282be155d078ad816d96
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-04-22 10:22:41 -0300

    refactor: changes the date examples

--- src/main/java/com/manager/freelancer_management_api/domain/global/entities/Deadline.java
+++ src/main/java/com/manager/freelancer_management_api/domain/global/entities/Deadline.java
@@ -2,16 +2,20 @@ package com.manager.freelancer_management_api.domain.global.entities;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Embeddable;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Embeddable
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class Deadline {
-    @Schema(example = "2025-04-10")
+    @Schema(example = "2028-05-10")
     private LocalDate startDate;
-    @Schema(example = "2025-05-10")
+    @Schema(example = "2028-06-10")
     private LocalDate endDate;
 
     public Deadline(LocalDate startDate, LocalDate endDate) {
