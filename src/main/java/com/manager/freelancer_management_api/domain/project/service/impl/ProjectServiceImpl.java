32400782f97aa634e4ffa02f410bb165e20f2d4a
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-05-08 18:49:20 -0300

    refactor: delegate project pagination and filtering logic to helper method

--- src/main/java/com/manager/freelancer_management_api/domain/project/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/manager/freelancer_management_api/domain/project/service/impl/ProjectServiceImpl.java
@@ -7,28 +7,26 @@ import com.manager.freelancer_management_api.domain.project.entity.Project;
 import com.manager.freelancer_management_api.domain.project.enums.ProjectStatus;
 import com.manager.freelancer_management_api.domain.project.repositories.ProjectRepository;
 import com.manager.freelancer_management_api.domain.project.service.IProjectService;
+import com.manager.freelancer_management_api.domain.project.utils.GetAllProjectsHelper;
 import com.manager.freelancer_management_api.domain.project.utils.ProjectAccessHelper;
 import com.manager.freelancer_management_api.domain.project.utils.ProjectUpdateHelper;
 import com.manager.freelancer_management_api.domain.user.entity.User;
 import com.manager.freelancer_management_api.domain.user.service.IUserService;
 import com.manager.freelancer_management_api.utils.validator.PasswordValidator;
 import com.manager.freelancer_management_api.utils.validator.UserAccessValidator;
-import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.*;
 import org.springframework.data.domain.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.function.Function;
 
 @Service
-@EnableCaching
 public class ProjectServiceImpl implements IProjectService {
-
-    private static final int DEFAULT_PAGE_SIZE = 10;
 
     private final ProjectRepository projectRepository;
     private final ProjectAccessHelper projectAccessHelper;
@@ -36,14 +34,16 @@ public class ProjectServiceImpl implements IProjectService {
     private final PasswordValidator passwordValidator;
     private final UserAccessValidator userAccessValidator;
     private final IUserService userService;
+    private final GetAllProjectsHelper getAllProjectHelper;
 
-    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectAccessHelper projectAccessHelper, ProjectUpdateHelper projectUpdateHelper, PasswordValidator passwordValidator, UserAccessValidator userAccessValidator, IUserService userService) {
+    public ProjectServiceImpl(ProjectRepository projectRepository, ProjectAccessHelper projectAccessHelper, ProjectUpdateHelper projectUpdateHelper, PasswordValidator passwordValidator, UserAccessValidator userAccessValidator, IUserService userService, GetAllProjectsHelper getAllProjectHelper) {
         this.projectRepository = projectRepository;
         this.projectAccessHelper = projectAccessHelper;
         this.projectUpdateHelper = projectUpdateHelper;
         this.passwordValidator = passwordValidator;
         this.userAccessValidator = userAccessValidator;
         this.userService = userService;
+        this.getAllProjectHelper = getAllProjectHelper;
     }
 
     @Override
@@ -60,20 +60,12 @@ public class ProjectServiceImpl implements IProjectService {
     @Transactional(readOnly = true)
     @PreAuthorize("isAuthenticated()")
     public Page<ProjectResponseDTO> getAllProjects(Pageable pageable) {
-        int pageSize = pageable.getPageSize() > 0 ? pageable.getPageSize() : DEFAULT_PAGE_SIZE;
-        Sort sort = pageable.getSort().isSorted() ? pageable.getSort() : Sort.by(Sort.Direction.DESC, "createdAt");
-
-        Pageable effectivePageable = PageRequest.of(pageable.getPageNumber(), pageSize, sort);
-
         List<ProjectStatus> statuses = Arrays.asList(ProjectStatus.OPEN, ProjectStatus.IN_PROGRESS);
 
-        Page<Project> projects = projectRepository.findByStatusIn(statuses, effectivePageable);
+        Function<Pageable, Page<Project>> fetcher = effectivePageable ->
+                projectRepository.findByStatusIn(statuses, effectivePageable);
 
-        List<ProjectResponseDTO> projectsList = projects.getContent().stream()
-                .map(project -> getProjectById(project.getId()))
-                .collect(Collectors.toList());
-
-        return new PageImpl<>(projectsList, projects.getPageable(), projects.getTotalElements());
+        return getAllProjectHelper.getAllProjects(pageable, fetcher);
     }
 
     @Override
