e0418a8dbeed5f7545d2538716800917b789d915
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-05-15 23:18:41 -0300

    refactor: move to global directory

---
+++ src/main/java/com/manager/freelancer_management_api/domain/global/exceptions/InvalidDateException.java
@@ -0,0 +1,7 @@
+package com.manager.freelancer_management_api.domain.global.exceptions;
+
+public class InvalidDateException extends BusinessException {
+    public InvalidDateException(String message) {
+        super(message);
+    }
+}
