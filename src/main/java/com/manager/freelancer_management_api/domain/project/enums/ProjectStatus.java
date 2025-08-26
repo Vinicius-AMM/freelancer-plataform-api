0d3f7433ec2693993e257b721f2fbeb32cc27c10
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-08-26 15:35:00 -0300

    feat: add new status for project.

--- src/main/java/com/manager/freelancer_management_api/domain/project/enums/ProjectStatus.java
+++ src/main/java/com/manager/freelancer_management_api/domain/project/enums/ProjectStatus.java
@@ -7,6 +7,8 @@ public enum ProjectStatus {
     OPEN("open"),
     NEGOTIATING("negotiating"),
     IN_PROGRESS("in_progress"),
+    COMPLETED_BY_FREELANCER("completed_by_freelancer"),
+    NEEDING_ADJUSTMENTS("needing_adjustments"),
     FINISHED("finished"),
     CANCELLED("cancelled");
 
