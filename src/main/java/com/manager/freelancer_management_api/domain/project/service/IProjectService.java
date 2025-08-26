3d9212c63d8919744dc4437cf31dcb0fb6ddc986
Vinicius-AMM <aurelioviniciusprofissional@gmail.com>
2025-08-26 15:32:51 -0300

    feat: implement new methods for project delivery flow.

--- src/main/java/com/manager/freelancer_management_api/domain/project/service/IProjectService.java
+++ src/main/java/com/manager/freelancer_management_api/domain/project/service/IProjectService.java
@@ -1,7 +1,6 @@
 package com.manager.freelancer_management_api.domain.project.service;
 
-import com.manager.freelancer_management_api.domain.project.dto.request.CreateProjectRequestDTO;
-import com.manager.freelancer_management_api.domain.project.dto.request.UpdateProjectRequestDTO;
+import com.manager.freelancer_management_api.domain.project.dto.request.*;
 import com.manager.freelancer_management_api.domain.project.dto.response.ProjectResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +11,13 @@ public interface IProjectService {
     ProjectResponseDTO getProjectById(Long projectId);
     void updateProject(Long projectId, UpdateProjectRequestDTO updateData);
     void deleteProject(Long projectId, String rawPassword);
+    void markProjectAsCompletedByFreelancer(Long projectId, ProjectCompletionRequestDTO completionRequest);
+    void approveProjectCompletionByClient(Long projectId, ApproveProjectRequestDTO approveProjectDTO);
+    void requestProjectAdjustments(Long projectId, ProjectAdjustmentRequestDTO adjustmentRequest);
+    Page<ProjectResponseDTO> getCompletedProjectsForClient(Pageable pageable);
+    Page<ProjectResponseDTO> getCompletedProjectsForFreelancer(Pageable pageable);
+    Page<ProjectResponseDTO> getProjectsPendingApprovalForClient(Pageable pageable);
+    Page<ProjectResponseDTO> getProjectsInProgressAndNeedingAdjustmentsForFreelancer(Pageable pageable);
+    Page<ProjectResponseDTO> getProjectsInProgressAndNeedingAdjustmentsForClient(Pageable pageable);
+    Page<ProjectResponseDTO> getProjectsNeedingAdjustmentsForFreelancer(Pageable pageable);
 }
